1302a0565423f5c1e60fc54a11d34be41881fa8e
char-lyy <711carlosalva771@gmail.com>
2025-08-19 13:41:58 -0300

    Clase Electrovalvula desarrollada

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
-
 public class Main {
+
+    public static void main(String[] args) {
+
+        Electrovalvula ev = new Electrovalvula();
+
+        System.out.println(ev.toString());
+
+        ev.abrir();
+
+        System.out.printf(ev.toString());
+
+    }
 }
